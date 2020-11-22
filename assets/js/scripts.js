571b2c0baac906c3fe39904813659b7c54bc95d2
Kopp <getkopp@gmail.com>
2020-11-22 23:16:11 +0800

    Updated Ahad 22 Nov 2020 11:16:11  +08

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,37 @@ menuTrigger.onclick = function() {
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+// Show profile
+
+var show = function (elem) {
+    elem.style.display = "block";
+  };
+  
+  var hide = function (elem) {
+    elem.style.display = "none";
+  };
+  
+  var toggle = function (elem) {
+    if (window.getComputedStyle(elem).display === "block") {
+      hide(elem);
+      return;
+    }
+  
+    show(elem);
+  };
+  
+  document.addEventListener(
+    "click",
+    function (event) {
+      if (!event.target.classList.contains("toggle")) return;
+  
+      event.preventDefault();
+  
+      var content = document.querySelector(event.target.hash);
+      if (!content) return;
+  
+      toggle(content);
+    },
+    false
+  );  
